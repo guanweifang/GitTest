cfca857104639282fa40265b01bedfc857e91e85
admin <admin@qq.com>
2022-06-15 14:46:49 +0800

    seconde commit

--- src/main/java/com/direct/controller.java
+++ src/main/java/com/direct/controller.java
@@ -3,5 +3,6 @@ package com.direct;
 public class controller {
     public static void main(String[] args) {
         System.out.println("git1===================");
+        System.out.println("git2=================");
     }
 }
