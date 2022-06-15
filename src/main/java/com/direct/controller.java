b3778fd1670caf1e8a9df9db6c440f87726aa2bc
admin <admin@qq.com>
2022-06-15 14:49:35 +0800

    by hot_fix commit2

--- src/main/java/com/direct/controller.java
+++ src/main/java/com/direct/controller.java
@@ -6,5 +6,6 @@ public class controller {
         System.out.println("git2=================");
         System.out.println("git3============");
         System.out.println("git4====================");
+        System.out.println("git5=============");
     }
 }
